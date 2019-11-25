4aebd4358f84788d5de9dcb480eb2d31822cb085
github username <github email address>
2019-11-25 17:02:21 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -8,10 +8,12 @@ class SongsController < ApplicationController
   end
 
   def new
+    @genre_options = Genre.all.pluck(:name, :id).to_a
     @song = Song.new
   end
 
   def create
+    puts params
     @song = Song.new(song_params)
 
     if @song.save
@@ -26,6 +28,7 @@ class SongsController < ApplicationController
   end
 
   def update
+    
     @song = Song.find(params[:id])
 
     @song.update(song_params)
@@ -47,7 +50,6 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title)
+    params.require(:song).permit(:title, :artist_name, :genre_id, notes_attributes: [:content])
   end
 end
-
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,14 @@
 class Song < ActiveRecord::Base
   # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+
+  def artist_name=(name)
+    self.artist = Artist.create(name: name)
+  end
+
+  def artist_name
+    self.artist.name if self.artist
+  end
 end
